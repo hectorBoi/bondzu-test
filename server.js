815ea28074260edb00ca35c177c9b5165ebad7e4
DulioCagg <duliop.caggiano@hotmail.com>
2020-06-21 14:39:49 -0500

    Adding base structure

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use(express.static("frontend"));
+
+const port = process.env.PORT || 8081;
+app.listen(port, () => console.log(`Listening in port ${port}`));
