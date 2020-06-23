958095ba2bb50f3bc3d6a1ebcfda86834d579b5e
Alejandro Lobato <a.lobato.molina@gmail.com>
2020-06-23 18:54:34 -0500

    Added initial files prior migration of the webpage.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
-app.use(express.static("frontend"));
+app.use(express.static("public"));
 
 const port = process.env.PORT || 8081;
 app.listen(port, () => console.log(`Listening in port ${port}`));
