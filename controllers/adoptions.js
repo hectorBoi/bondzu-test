b74db9ab6cc211b59c5b7cd13021f54d6cf5a864
DulioCagg <duliop.caggiano@hotmail.com>
2020-07-14 14:27:23 -0500

    Completed the adoptions route

--- controllers/adoptions.js
+++ controllers/adoptions.js
@@ -1,6 +1,24 @@
-//TODO
+const animalInfo = require("./animalInfo");
+
 const handleAdoptions = async (req, res, Parse) => {
-  res.json("Adoptions!")
+  const { username } = req.body; // DEBERIA DE SER HEADER
+
+  try {
+    const userTable = Parse.Object.extend("User");
+    const query = new Parse.Query(userTable);
+    query.equalTo("username", username)
+    const user = await query.first();
+
+    const adoptions = user.get("adoptersRelation");
+    const queryAD = adoptions.query();
+    queryAD.equalTo("isActive", true);
+    const activeAdoptions = await queryAD.find();
+    const adoptionsInfo = await animalInfo.getAnimalInfo(activeAdoptions, Parse);
+
+    res.json(adoptionsInfo)
+  } catch (err) {
+    res.status(400).json("Adoptions did not worked")
+  }
 }
 
 module.exports = {
--- controllers/animals.js
+++ controllers/animals.js
@@ -1,57 +1,4 @@
-// Get the name of the zoo in charge of a specific animal
-const getKeeper = async (id, Parse) => {
-  try {
-    const keepersTable = Parse.Object.extend("Keeper");
-    const queryKeeper = new Parse.Query(keepersTable);
-    const keeperObj = await queryKeeper.get(id);
-    const keeperZoo = keeperObj.get("zoo");
-
-    const zooTable = Parse.Object.extend("Zoo");
-    const queryZoo = new Parse.Query(zooTable);
-    const zoo = await queryZoo.get(keeperZoo.id);
-    const zooName = zoo.get("name");
-    return zooName;
-  } catch (err) {
-    return "Didnt find keeper"
-  }
-}
-
-const getVideo = async (id, Parse) => {
-  try {
-    const videoTable = Parse.Object.extend("Video");
-    const query = new Parse.Query(videoTable);
-    const camera = await query.find();
-    const filter = camera.filter(camara => camara.get("animal_id").id === id)
-    if (filter[0]) {
-      return filter[0].get("youtube_ids")[0]
-    }
-    return "No url"
-  } catch (err) {
-    return err
-  }
-}
-
-// Transforms the array of Parse.Objects into Json
-const getAnimalInfo = async (array, Parse) => {
-  let animalInfo = await Promise.all(array.map(async animal => {
-    const keeper = await getKeeper(animal.get("keepers")[0].id, Parse);
-    const video = await getVideo(animal.id, Parse);
-    return animal = {
-      id: animal.id,
-      youtubeID: video,
-      keeper: keeper,
-      profilePhoto: animal.get("profilePhoto")._url,
-      name: animal.get("name"),
-      about: animal.get("about"),
-      characteristics: animal.get("characteristics"),
-      species: animal.get("species"),
-      userType: animal.get("animalRequiredPriority").id,
-    }
-  }
-  ));
-
-  return animalInfo;
-}
+const animalInfo = require("./animalInfo");
 
 // Filter the array of animals depending on the type of the user
 const filterAnimals = (userType, animals) => {
@@ -82,7 +29,7 @@ const handleAnimals = async (req, res, Parse) => {
     const query = new Parse.Query(animalTable);
     query.equalTo("isActive", true);
     const activeAnimals = await query.find();
-    const animalsInfo = await getAnimalInfo(activeAnimals, Parse);
+    const animalsInfo = await animalInfo.getAnimalInfo(activeAnimals, Parse);
     const result = filterAnimals(usertype, animalsInfo)
     res.json(result);
   } catch (err) {
