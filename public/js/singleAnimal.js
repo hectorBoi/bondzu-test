ef1635c7c7fedb5777737c435ac96fe3144f7c2f
AndresBeriss <abr1499@gmail.com>
2020-08-10 18:30:14 -0500

    singleAnimal.js test for dynamic load singleAnimal page

---
+++ public/js/singleAnimal.js
@@ -0,0 +1,92 @@
+const container = document.getElementById("container");
+
+const createDiv = (className) => {
+  let div = document.createElement("div");
+  div.className = className;
+
+  return div;
+};
+
+const createRow = () => {
+  let div = createDiv("row");
+
+  return div;
+};
+
+const createIframe = (youtubeID) => {
+  const col = createDiv("col-xl-8");
+  const div = createDiv("embed-responsive embed-responsive-16by9");
+  div.style.marginTop = "15px";
+  col.appendChild(div);
+
+  const iframe = document.createElement("iframe");
+  iframe.setAttribute("src", "https://www.youtube.com/embed/" + youtubeID);
+  img.className = "embed-responsive-item";
+  iframe.allowFullscreen = true;
+  div.appendChild(iframe);
+
+  return col;
+};
+
+const createCard = (object) => {
+  const col = createDiv("col-xl-4");
+
+  const card = createDiv("card");
+  card.style.marginTop = "15px";
+  col.appendChild(card);
+
+  const img = document.createElement("img");
+  img.setAttribute("src", object.profilePhoto);
+  img.className = "card-img img-fluid";
+  img.setAttribute("alt", "animal");
+  img.setAttribute("height", "300px");
+  card.appendChild(img);
+
+  const button = document.createElement("button");
+  button.className = "btn btn-success btn-lg btn-block";
+  button.setAttribute("type", "button");
+  button.innerHTML = "¡Adóptame!";
+  card.appendChild(button);
+
+  const cardBody = createDiv("card-body");
+  card.appendChild(cardBody);
+
+  const header4 = document.createElement("h4");
+  header4.className = "card-title text-center";
+  header4.innerHTML = object.name;
+  cardBody.appendChild(header4);
+
+  const list = document.createElement("ul");
+  list.className = "list-group list-group-flush";
+  cardBody.appendChild(list);
+
+  const aboutItem = createListItem("Acerca de", object.about);
+  list.appendChild(aboutItem);
+
+  const characteristcsItem = createListItem(
+    "Características",
+    object.characteristcs
+  );
+  list.appendChild(characteristcsItem);
+
+  const speciesItem = createListItem("Especie", object.species);
+  list.appendChild(speciesItem);
+
+  const keeperItem = createListItem("Cuidador", object.keeper);
+  list.appendChild(keeperItem);
+
+  return col;
+};
+
+const createListItem = (title, info) => {
+  const listIteam = document.createElement("li");
+  listIteam.className = "list-group-item";
+
+  const header5 = document.createElement("h5");
+  header5.className = "card-title";
+  header5.innerHTML = title;
+  listIteam.appendChild(header5);
+  listIteam.innerHTML = info;
+
+  return listIteam;
+};
