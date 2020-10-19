24d4c6564c245c7b0879d52bc0c59cbe54af6908
Andrés <abr1499@gmail.com>
2020-10-19 16:59:49 -0500

    newColleague script created/ unused attributes removed from newCollegue page and newAnimal script

--- public/admin/js/newColleague.js
+++ public/admin/js/newColleague.js
@@ -13,24 +13,14 @@ const animalPhotoElem = document.getElementById("animalPhoto");
 const statusElem = document.getElementById("status");
 
 //Datos en español
-const especieElem = document.getElementById("especie");
+const nombreElem = document.getElementById("nombre");
 const acercaElem = document.getElementById("acerca");
-const nombreCientElem = document.getElementById("nomCient");
-const estadoConElem = document.getElementById("estadoCon");
-const distGeoElem = document.getElementById("distGeo");
-const habitatEspElem = document.getElementById("habitatEsp");
-const dietaElem = document.getElementById("dieta");
-const reproduccionElem = document.getElementById("reproduccion");
+const caracteristicasElem = document.getElementById("caracteristicas");
 
 //English data
-const speciesElem = document.getElementById("species");
+const nameElem = document.getElementById("name");
 const aboutElem = document.getElementById("about");
-const scieNameElem = document.getElementById("scieName");
-const conStatusElem = document.getElementById("conStatus");
-const geoDistElem = document.getElementById("geoDist");
-const habitatEngElem = document.getElementById("habitatEng");
-const dietElem = document.getElementById("diet");
-const reproductionElem = document.getElementById("reproduction");
+const characteristicsElem = document.getElementById("characteristics");
 
 //Keepers
 const allKeepersElem = document.getElementById("allKeepers");
@@ -69,76 +59,36 @@ submitSaveElem.addEventListener("click", () => {
   const keeper = allKeepersElem.options[allKeepersElem.selectedIndex].value;
 
   //Información español
-  const especie = especieElem.value;
+  const nombre = nombreElem.value;
   const acerca = acercaElem.value;
-  const nombreCient = nombreCientElem.value;
-  const estadoCon = estadoConElem.value;
-  const distGeo = distGeoElem.value;
-  const habitatEsp = habitatEspElem.value;
-  const dieta = dietaElem.value;
-  const reproduccion = reproduccionElem.value;
+  const caracteristicas = caracteristicasElem.value;
 
   //English information
-  const species = speciesElem.value;
+  const name = nameElem.value;
   const about = aboutElem.value;
-  const scieName = scieNameElem.value;
-  const conStatus = conStatusElem.value;
-  const geoDist = geoDistElem.value;
-  const habitatEng = habitatEngElem.value;
-  const diet = dietElem.value;
-  const reproduction = reproductionElem.value;
+  const characteristics = characteristicsElem.value;
 
   if (
     youtubeId != "" &&
-    especie != "" &&
+    nombre != "" &&
     acerca != "" &&
-    nombreCient != "" &&
-    estadoCon != "" &&
-    distGeo != "" &&
-    habitatEsp != "" &&
-    dieta != "" &&
-    reproduccion != "" &&
-    species != "" &&
+    caracteristicas != "" &&
+    name != "" &&
     about != "" &&
-    scieName != "" &&
-    conStatus != "" &&
-    geoDist != "" &&
-    habitatEng != "" &&
-    diet != "" &&
-    reproduction != "" &&
-    keeper != ""
+    keeper != "" &&
+    characteristics != ""
   ) {
     buttonSpinnerElem.removeAttribute("style");
 
-    //Características español
-    const caracteristicas = {
-      "Nombre científico": nombreCient,
-      "Estado de conservación": estadoCon,
-      "Distribución geográfica": distGeo,
-      Hábitat: habitatEsp,
-      Dieta: dieta,
-      Reproducción: reproduccion,
-    };
-
-    //English characteristics
-    const characteristics = {
-      "Scientific name": scieName,
-      "Conservation status": conStatus,
-      "Geographical distribution": geoDist,
-      Habitat: habitatEng,
-      Diet: diet,
-      Reproduction: reproduction,
-    };
-
     const request = {
-      name: nombreCient,
-      name_en: scieName,
+      name: nombre,
+      name_en: name,
       about: acerca,
       about_en: about,
       characteristics: caracteristicas,
       characteristics_en: characteristics,
-      species: especie,
-      species_en: species,
+      species: "Colega",
+      species_en: "Colleague",
       youtubeID: youtubeId,
       keeper: keeper,
       isActive: status,
