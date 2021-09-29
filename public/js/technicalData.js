c6e742b0995fbd1dbf776d60f9db183b1d178f6d
fernandonietoms <fernando.nietoms@gmail.com>
2021-09-29 15:00:00 -0500

    Add the reloadIFrame function to technicalData.js

--- public/js/technicalData.js
+++ public/js/technicalData.js
@@ -11,6 +11,14 @@ const loaderElements = document.getElementById("loaderElements");
 const body = document.getElementById("body");
 body.style.overflow = "hidden";
 
+function reloadIFrame(){
+  if(iframeElem.src.includes("https://vauth.command.verkada.com/embed/html/d9f30996-c827-4303-ac25-cc41b2c8cb78/")){
+    iframeElem.src = `https://command.verkada.com/embed.html#%7B%22entityId%22%3A%20%2255054a74-f45b-40ec-b621-cf7efef51177%22%2C%20%22token%22%3A%20%22v2_83f8442645b8ea03c62e2dd6d286a188%22%2C%20%22urlSD%22%3A%20%22https%3A//vstream.command.verkada.com/filter/transcode/v2_83f8442645b8ea03c62e2dd6d286a188/55054a74-f45b-40ec-b621-cf7efef51177/low_res/livevideo.m3u8%22%2C%20%22urlHD%22%3A%20%22https%3A//vfilter.command.verkada.com/filter/transcode/v2_83f8442645b8ea03c62e2dd6d286a188/55054a74-f45b-40ec-b621-cf7efef51177/high_res/livevideo.m3u8%22%7D`;
+  }
+  
+  iframeElem.src = iframeElem.src;
+}
+
 fetch(`/animals/technicalData/${animalID}`)
   .then((res) => res.json())
   .then((animal) => {
@@ -19,6 +27,7 @@ fetch(`/animals/technicalData/${animalID}`)
     // Checks if the camera is from youtube or another page
     if (animal.youtubeID.includes("http")) {
       youtubeURL = animal.youtubeID;
+      reloadIFrame();
       //console.log("youtubeURL");
       //console.log(youtubeURL);
     } else {
