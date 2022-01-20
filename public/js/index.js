0cc34aa1ea4c180c6bb78bea294228ae7995f7db
Fernando <flopezlopez6@outlook.com>
2022-01-19 19:56:36 -0600

    lastLoginWeb added when loggedin by cookie

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,42 @@
-const guest = document.getElementById("guest");
-const animals = document.getElementById("navAnimals");
-const loggedIn = document.getElementById("loggedIn");
-const cardLogin = document.getElementById("cardLogin");
-const conocenos = document.getElementById("conocenos");
+const guest = document.getElementById('guest');
+const animals = document.getElementById('navAnimals');
+const loggedIn = document.getElementById('loggedIn');
+const cardLogin = document.getElementById('cardLogin');
+const conocenos = document.getElementById('conocenos');
 
-if (!document.cookie.includes("token")) {
-  animals.style.display = "none";
-  loggedIn.style.display = "none";
+if (!document.cookie.includes('token')) {
+  animals.style.display = 'none';
+  loggedIn.style.display = 'none';
 } else {
-  guest.style.display = "none";
+  guest.style.display = 'none';
   if (cardLogin) {
-    cardLogin.style.display = "none";
-    conocenos.className = "";
+    cardLogin.style.display = 'none';
+    conocenos.className = '';
   }
+  let cookie = {};
+  document.cookie.split(';').forEach(function (el) {
+    let [key, value] = el.split('=');
+    cookie[key.trim()] = value;
+  });
+  // Sets last login date
+  fetch('/reports/lastLoginWeb/', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: cookie.username,
+    }),
+  })
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 //Preloading page
-window.addEventListener("load", function () {
-  const loader = document.getElementById("loader");
-  loader.className += " hidden";
+window.addEventListener('load', function () {
+  const loader = document.getElementById('loader');
+  loader.className += ' hidden';
   loader.style.display = 'none';
 });
