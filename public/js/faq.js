89931615e1c64d6fd262efd10ec922d42824232e
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2023-08-08 14:04:42 -0600

    changes in animals view

--- public/js/faq.js
+++ public/js/faq.js
@@ -7,7 +7,7 @@ document.getElementById("faq").innerHTML = `
       aria-labelledby="faqModalLabel"
       aria-hidden="true"
     >
-      <div class="modal-dialog">
+      <div class="modal-dialog modal-xl">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="faqModalLabel">
